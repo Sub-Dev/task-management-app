bb8f3e8f3b134d72c82cc242bdea183fcf37d6fb
SubDev <bilomarin@gmail.com>
2024-07-18 17:21:35 -0300

    adicioção de entity do banco de ados postqSql necessarios e criação de alguns componentes do backend para user como controller,dto,module e service e modificação readme

---
+++ backend/src/user/user.dto.ts
@@ -0,0 +1,17 @@
+// src/users/user.dto.ts
+
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,47 @@
+// src/users/users.service.ts
+
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findById(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(newUser);
+  }
+
+  async update(id: number, updateUserDto: Partial<CreateUserDto>): Promise<User> {
+    const user = await this.findById(id);
+    this.userRepository.merge(user, updateUserDto);
+    return await this.userRepository.save(user);
+  }
+
+  async delete(id: number): Promise<void> {
+    const user = await this.findById(id);
+    await this.userRepository.remove(user);
+  }
+}
